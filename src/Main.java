2240891e44729f9906a551eda5c16e7ccac5726e
Yuriy Sviridov <crip.shooter@gmail.com>
2022-07-26 09:09:55 +0300

    Switch int to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{10,15,50,30});
+        SalesManager salesManager = new SalesManager(new long[]{10,15,50,30});
         salesManager.max();
         System.out.println(salesManager);
         System.out.println("Класс!");
